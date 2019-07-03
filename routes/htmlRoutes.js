a3b9d0e2c27753da8d46fb69c5c417a4b388bb30
vsbakula <goldenrhesus2022@gmail.com>
2019-07-02 19:51:12 -0700

    setup handlebars

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,10 +1,10 @@
 var db = require("../models");
 var path = require("path");
 
-module.exports = function(app) {
+module.exports = function (app) {
   // Load index page
-  app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
+  app.get("/", function (req, res) {
+    db.Example.findAll({}).then(function (dbExamples) {
       res.render("index", {
         msg: "Welcome!",
         examples: dbExamples
@@ -12,9 +12,14 @@ module.exports = function(app) {
     });
   });
 
+  app.get("/signup", function (req, res) {
+
+    res.render("signup");
+
+  });
   // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+  app.get("/example/:id", function (req, res) {
+    db.Example.findOne({ where: { id: req.params.id } }).then(function (dbExample) {
       res.render("example", {
         example: dbExample
       });
@@ -22,7 +27,7 @@ module.exports = function(app) {
   });
 
   // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
+  app.get("*", function (req, res) {
     res.render("404");
   });
 };
